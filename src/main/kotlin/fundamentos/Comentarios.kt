f2463c326396438f9071d15c619e080b480d5d02
Athay <abdieldeathayde67@gmail.com>
2023-10-01 20:38:42 -0300

    Atualizando curso Cod3r

---
+++ src/main/kotlin/fundamentos/Comentarios.kt
@@ -0,0 +1,25 @@
+package fundamentos
+
+/**
+ * Só para dizer que o Kotlin suporta o KDoc... :)
+ *
+ * @param args lista de parâmetros passados por linha de comando
+ */
+fun main() {
+    // Comentário de uma linha
+    println("Opa")
+
+    /*
+        Mais de
+        uma linha
+     */
+    println("Sério?")
+
+    /*
+    * Mais de
+    * uma linha
+    * com estilo
+     */
+    println("Legal!")
+
+}
